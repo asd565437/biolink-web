73a799a99a2298e1af11dbf3ab93fc06a72dfe79
沈羿伶 <asd565437@Mac.mshome.net>
2025-02-18 22:52:20 +0800

    Convert Firebase module to CommonJS

--- src/js/Confirm.js
+++ src/js/Confirm.js
@@ -2,6 +2,7 @@ import "../css/Confirm.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import Header from "./Header.js";
+import axios from "axios";
 import confirm_title from "../confirm/confirm_title.svg";
 import confirm_back from "../confirm/confirm_back.svg";
 import confirm_test from "../confirm/confirm_test.svg";
@@ -13,32 +14,29 @@ const Confirm = () => {
     const location = useLocation();
     const friendId = location.state?.friendId || "未知用户";
     const [invitations, setInvitations] = useState([]);
+    const [photo, setPhoto] = useState(confirm_test);
     const navigate = useNavigate();
     const socket = useContext(SocketContext); // 使用全局 socket
     const userId = useContext(UserContext);
     useEffect(()=>{
         const handleFriend = async () => {
             try {
-                const response = await fetch(`${apiUrl}/api/get-friend-info`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        id: userId,
-                    }),
+                const response = await axios.post(`${apiUrl}/api/get-friend-info`, {
+                    id: friendId,
                 });
-                if (response.ok) {
-                    alert("註冊成功!");
-                    console.log(response.data);
-                } else {
-                    const errorData = await response.json();
-                    alert("註冊失敗：" + errorData.error);
-                }
+                setPhoto(response.data.player.photoURL)
+                console.log(photo);
             } catch (error) {
-                alert("網路錯誤，請稍後再試！");
-                console.error(error);
-            }
+                if (error.response) {
+                    // 服务器返回错误
+                    alert("邀請失敗：" + error.response.data.error);
+                    console.error("响应错误:", error.response.data);
+                } else {
+                    // 网络错误或其他问题
+                    alert("请求失败：" + error.message);
+                    console.error("请求失败:", error);
+                }
+            }            
         };
         handleFriend();
     })
@@ -73,7 +71,7 @@ const Confirm = () => {
             <main className="content">
                 <img src={confirm_back} alt="confirm_back" className="confirm_back" onClick={() => navigate(-1)} />
                 <img src={confirm_title} alt="confirm_title" className="confirm_title" />
-                <img src={confirm_test} alt="confirm_test" className="confirm_test" />
+                <img src={photo} alt="confirm_test" className="confirm_test" />
                 <img src={confirm_start} alt="confirm_start" className="confirm_start"  />
                 <h3>收到的好友邀请：</h3>
                 {invitations.length > 0 ? invitations.map((invite, index) => (
