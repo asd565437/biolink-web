38f6a69113d4b0f278fc96b51acdc2d2d60a3908
沈羿伶 <asd565437@Mac.mshome.net>
2025-02-23 17:27:58 +0800

    Convert Firebase module to CommonJS

--- src/js/Friend.js
+++ src/js/Friend.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "../css/Friend.css"; // 引入样式文件
 import Header from "./Header.js";
